868ea241db2f1ee48dd892687a24dcb78d3dc4f3
jacobpalaoro <jpalaoro197@gmail.com>
2022-09-09 11:00:57 -0700

    add coffee.js with proper fields.

---
+++ src/Components/Coffee.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Coffee(props){
+  return (
+    <React.Fragment>
+      <h3>{props.name} - {props.roast}</h3>
+      <h3>{props.price}</h3>
+      <p><em>{props.origin}</em></p>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+Coffee.propTypes = {
+  roast: PropTypes.string,
+  name: PropTypes.string,
+  origin: PropTypes.string,
+  price: PropTypes.number
+};
+
+export default Coffee;
