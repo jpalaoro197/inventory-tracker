5514179dbe1d540018a1ca5dd27c00142c1b4cbc
jacobpalaoro <jpalaoro197@gmail.com>
2022-09-09 13:04:02 -0700

    add on click for coffee details to coffee.js.

--- src/Components/Coffee.js
+++ src/Components/Coffee.js
@@ -4,11 +4,13 @@ import PropTypes from "prop-types";
 function Coffee(props){
   return (
     <React.Fragment>
+      <div onClick = {() => props.whenCoffeeClicked(props.id)}>
       <h3>{props.name} - {props.roast}</h3>
       <h3>${props.price}</h3>
       <p><em>Bean Origin{props.origin}</em></p>
       <p><em>Pounds Remaining:{props.stock}</em></p>
       <hr/>
+      </div>
     </React.Fragment>
   );
 }
@@ -19,6 +21,8 @@ Coffee.propTypes = {
   origin: PropTypes.string,
   price: PropTypes.number,
   stock: PropTypes.number,
+  id: PropTypes.string,
+  whenCoffeeClicked: Proptypes.func
 };
 
 export default Coffee;
