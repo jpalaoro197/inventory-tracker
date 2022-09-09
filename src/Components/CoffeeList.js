3422bbb369525e4087c895f8e559a5e26662352d
jacobpalaoro <jpalaoro197@gmail.com>
2022-09-09 15:26:29 -0700

    rebuild coffeelist.js

--- src/Components/CoffeeList.js
+++ src/Components/CoffeeList.js
@@ -1,28 +1,25 @@
-import React from "react";
+import React from 'react';
 import Coffee from "./Coffee";
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
-
-function CoffeeList(props) { // Add props as parameter.
+function CoffeeList(props){
   return (
     <React.Fragment>
-      <hr />
-      {props.coffeeList.map((coffee) => // Loop through the list passed down from CoffeeControl.
-        <Coffee
-        whenCoffeeClicked={props.onCoffeeSelection}
-         roast={coffee.roast}
-          name={coffee.name}
-          origin={coffee.origin}
-          price={coffee.price}
-          stock={coffee.stock}
-          key={coffee.id}
-          id={coffee.id} />
-      )}
+      <hr/>
+            {props.coffeeList.map((coffee) =>
+              <Coffee
+                whenCoffeeClicked = { props.onCoffeeSelection }
+                name={coffee.name}
+                roast={coffee.roast}
+                stock={coffee.stock}
+                id={coffee.id}
+                key={coffee.id}/>
+            )}
+      
     </React.Fragment>
   );
 }
 
-// Add propTypes for coffeeList.
 CoffeeList.propTypes = {
   coffeeList: PropTypes.array,
   onCoffeeSelection: PropTypes.func
